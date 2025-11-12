1841ac87ba4c6e691bcc6b2e355d285917d6a2e0
Veronika Matos <veronikamatos@ufl.edu>
2025-11-11 21:55:07 -0500

    updated function.cs and adding the other files for webhook

---
+++ Function.cs
@@ -0,0 +1,47 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace EsepWebhook;
+
+public class Function
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input">The event for the Lambda function handler to process.</param>
+    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
+    /// <returns></returns>
+    public string FunctionHandler(string input, ILambdaContext context) {
+        context.Logger.LogInformation($"FunctionHandler received: {input}");
+
+        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+
+        // Testing from postman, you can use this code to test the function
+        /*
+        context.Logger.LogInformation($"Body: {json.body}");
+        dynamic body = JsonConvert.DeserializeObject<dynamic>(json.body.ToString());
+        context.Logger.LogInformation($"Issue: {body.issue}");
+        context.Logger.LogInformation($"Html: {body.issue.html_url}");
+        string payload = $"{{'text':'Issue Created: {body.issue.html_url}'}}";
+        */
+        
+        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+
+        var client = new HttpClient();
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        {
+            Content = new StringContent(payload, Encoding.UTF8, "application/json")
+        };
+    
+        var response = client.Send(webRequest);
+        using var reader = new StreamReader(response.Content.ReadAsStream());
+            
+        return reader.ReadToEnd();
+    }
+    // {
+    //     // return input.ToUpper();
+    // }
+}
